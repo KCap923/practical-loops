d4e27909198c925408604dda607fb8aaeabbb3f3
Krystle Capuano <krystlecapuano@gmail.com>
2024-04-13 16:28:27 -0400

    "Buzz"

--- script.js
+++ script.js
@@ -8,7 +8,13 @@
 
 // Fizz Buzz
 
-for (let i = 1; i < 100; i++) {
+for (let i = 0; i < 101; i++) {
 if (i % 3 === 0) {
-  console.log(i); 
-}}
+  console.log(i + " - Fizz"); 
+}
+else if (i % 5 === 0) {
+  console.log(i + " - Buzz")
+}
+else ( i)
+
+}
