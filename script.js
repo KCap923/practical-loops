ef706efdc3241b82485dfa9b2a41519f47c24033
Krystle Capuano <krystlecapuano@gmail.com>
2024-04-13 16:24:15 -0400

    "Fizz"

---
+++ script.js
@@ -0,0 +1,14 @@
+//alert ("Cool chicks rise up!")
+
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, 
+
+// Fizz Buzz
+
+for (let i = 1; i < 100; i++) {
+if (i % 3 === 0) {
+  console.log(i); 
+}}
