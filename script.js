ad621f54841853b839480df5cb8c684fe39088e2
Krystle Capuano <krystlecapuano@gmail.com>
2024-04-14 18:46:06 -0400

    ''Prime time "

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 // Loop through all numbers from 1 to 100.
 // If a number is divisible by 3, log “Fizz.”
 // If a number is divisible by 5, log “Buzz.”
-// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
-// If a number is not divisible by either 3 or 5, 
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.” 
 
 // Fizz Buzz
 
@@ -15,6 +14,34 @@ if (i % 3 === 0) {
 else if (i % 5 === 0) {
   console.log(i + " - Buzz")
 }
-else ( i)
+else if (i % 3 === 0 && i % 5 === 0 ) {
+  console.log(i + " - Fizz- Buzz!!!")
+}
+}
+
+console.log("___________")
+
+
+
+//Prime Time
+
+let n = 31;
+let nextNum = 47
+
+for (let i = n; i <= nextNum; i++) {
+  let flag = 0; 
+
+for (let j = 2; j < i; j++) {
+  if (i % j === 0) {
+    flag =1;
+      break;
+  }
+}
+if (flag === 0) {
+  console.log(`${i} is prime!`);
+}
+
 
 }
+
+console.log(`${i} is prime!`)
