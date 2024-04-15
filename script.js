47b6bbceea650270b0f85e7877ee179f0a77415a
Krystle Capuano <krystlecapuano@gmail.com>
2024-04-14 20:28:16 -0400

    "Differnt prime numbers"

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 //alert ("Cool chicks rise up!")
 
-// Loop through all numbers from 1 to 100.
-// If a number is divisible by 3, log “Fizz.”
-// If a number is divisible by 5, log “Buzz.”
-// If a number is divisible by both 3 and 5, log “Fizz Buzz.” 
-
 // Fizz Buzz
 
-for (let i = 0; i < 101; i++) {
+for (let i = 0; i < 15; i++) {
 if (i % 3 === 0) {
   console.log(i + " - Fizz"); 
 }
@@ -18,21 +13,29 @@ else if (i % 3 === 0 && i % 5 === 0 ) {
   console.log(i + " - Fizz- Buzz!!!")
 }
 }
+// Gave results for 0 -  15 only for a shorter log, but did test 0 - 100
+//-------> 
+//  0 - Fizz
+//  3 - Fizz
+//  5 - Buzz
+//  6 - Fizz
+//  9 - Fizz
+//  10 - Buzz
+//  12 - Fizz
+
 
 console.log("___________")
 
-
-
 //Prime Time
 
-let n = 31;
-let nextNum = 47
+let n = 701;
+let nextNum = 730;
 
 for (let i = n; i <= nextNum; i++) {
   let flag = 0; 
 
-for (let j = 2; j < i; j++) {
-  if (i % j === 0) {
+for (let p = 2; p < i; p++) {
+  if (i % p === 0) {
     flag =1;
       break;
   }
@@ -40,8 +43,22 @@ for (let j = 2; j < i; j++) {
 if (flag === 0) {
   console.log(`${i} is prime!`);
 }
-
-
 }
 
-console.log(`${i} is prime!`)
+// The class work example was helpful!
+//-------> 
+//  701 is prime!
+//  709 is prime!
+//  719 is prime!
+//  727 is prime!
+
+console.log("___________")
+
+// Feeling Loopy
+
+ID,Name,Occupation,Age\n
+42,Bruce,Knight,41\n
+57,Bob,Fry Cook,19\n
+63,Blaine,Quiz Master,58\n
+98,Bill,Doctor’s Assistant,26
+
